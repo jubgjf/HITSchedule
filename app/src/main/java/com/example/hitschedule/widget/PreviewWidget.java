57623ad9f8e2816b630153069a3b7ffc738939c2
Guan Jiannan <guanjiannan@outlook.com>
2020-11-04 09:15:07 +0800

    支持手动更新widget按钮

--- app/src/main/java/com/example/hitschedule/widget/PreviewWidget.java
+++ app/src/main/java/com/example/hitschedule/widget/PreviewWidget.java
@@ -25,16 +25,32 @@ public class PreviewWidget extends AppWidgetProvider {
      */
     private static final String REFRESH_BUTTON_CLICKED = "RefreshButtonClick";
 
+    /**
+     * 课程时间表
+     */
+    private static final String[] courseTimes = new String[]{
+            "8:00", "9:45", "10:00", "11:45",
+            "13:45", "15:30", "15:45", "17:30",
+            "18:30", "20:15", "20:30", "22:15"
+    };
+
+    /**
+     * 所有小组件的ID
+     */
+    private int[] widgetIds;
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
 
+        widgetIds = appWidgetIds;
+
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_preview);
         ComponentName watchWidget = new ComponentName(context, PreviewWidget.class);
 
-        remoteViews.setOnClickPendingIntent(R.id.refresh_button, getPendingSelfIntent(context));
+        remoteViews.setOnClickPendingIntent(R.id.refresh_button, getPendingIntent(context));
         appWidgetManager.updateAppWidget(watchWidget, remoteViews);
     }
 
@@ -42,26 +58,18 @@ public class PreviewWidget extends AppWidgetProvider {
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
 
-        if (REFRESH_BUTTON_CLICKED.equals(intent.getAction())) {
-            // TODO 手动更新widget
-//            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-//
-//            RemoteViews remoteViews;
-//            ComponentName watchWidget;
-//
-//            remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_preview);
-//            watchWidget = new ComponentName(context, PreviewWidget.class);
-//
-//            remoteViews.setTextViewText(R.id.refresh_button, "TESTING");
-//
-//            appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+        if (REFRESH_BUTTON_CLICKED.equals(intent.getAction())) {    // 手动更新widget
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            for (int appWidgetId : widgetIds) {
+                updateAppWidget(context, appWidgetManager, appWidgetId);
+            }
         }
     }
 
     /**
      * 获取点击refresh_button的PendingIntent
      */
-    private PendingIntent getPendingSelfIntent(Context context) {
+    private PendingIntent getPendingIntent(Context context) {
         Intent intent = new Intent(context, getClass());
         intent.setAction(PreviewWidget.REFRESH_BUTTON_CLICKED);
         return PendingIntent.getBroadcast(context, 0, intent, 0);
@@ -71,11 +79,19 @@ public class PreviewWidget extends AppWidgetProvider {
      * 刷新小组件
      */
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        // TODO 清空旧内容
         RemoteViews containerViews = new RemoteViews(context.getPackageName(), R.layout.widget_preview);
 
         for (Schedule course : getTodayCourses()) {
             RemoteViews elementViews = new RemoteViews(context.getPackageName(), R.layout.widget_preview_element);
+
+            // 课程名
             elementViews.setTextViewText(R.id.course_textview, course.getName());
+
+            // 课程时间
+            String courseTimeSpan = courseTimes[course.getStart() - 1] + "-" + courseTimes[course.getStart() + course.getStep() - 2];
+            elementViews.setTextViewText(R.id.course_time_textview, courseTimeSpan);
+
             containerViews.addView(R.id.widget_container, elementViews);
         }
 
